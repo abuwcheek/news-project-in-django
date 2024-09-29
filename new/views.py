9a56b95cf8095cd742fd9361bacdcfd59ee7f1f3
abuwcheek <abdulloistamov1034@gmail.com>
2024-09-29 23:02:29 +0500

    views.py tayyorlanmoqda

--- new/views.py
+++ new/views.py
@@ -6,7 +6,7 @@ from .models import CategoryModel, TagModel, NewModel, AboutModel, ContactModel,
 
 class CategoryModelListView(View):
      def get(self, request):
-          ctg_list = CategoryModel.objects.all().filter(is_active=True)
+          ctg_list = CategoryModel.objects.all()
 
           context = {
                'ctg_list': ctg_list,
@@ -17,4 +17,19 @@ class CategoryModelListView(View):
 
 class IndexView(View):
      def get(self, request):
-          return render(request, 'index.html')
+          news = NewModel.objects.all().filter(is_active=True).order_by('?')[:6]
+          sports = NewModel.objects.filter(category__title='Sports')[:3]
+          technology = NewModel.objects.filter(category__title='Technology')[:3]
+          business = NewModel.objects.filter(category__title='Business')[:3]
+          entertainment = NewModel.objects.filter(category__title='Entertainment')[:3]
+          political_news = NewModel.objects.filter(category__title='	Political news')[:3]
+
+          context = {
+               'news': news,
+               'sports': sports,
+               'technology': news,
+               'business': business,
+               'entertainment': entertainment,
+               'political_news': political_news,
+          }
+          return render(request, 'index.html', context)
