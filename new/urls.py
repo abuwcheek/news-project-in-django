d906d11bb030d75030bf241dc9b1a892ae967f3f
abuwcheek <abdulloistamov1034@gmail.com>
2024-09-29 15:13:47 +0500

    index, base added

--- new/urls.py
+++ new/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-
-
+from .views import IndexView
 
 
 urlpatterns = [
-
+     path('list/', IndexView.as_view(), name='index')
 ]
--- new/views.py
+++ new/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views import View
+from .models import CategoryModel, TagModel, NewModel, AboutModel, ContactModel, AdsModel
 
-# Create your views here.
+
+
+class CategoryModelListView(View):
+     def get(self, request):
+          ctg_list = CategoryModel.objects.all().filter(is_active=True)
+
+          context = {
+               'ctg_list': ctg_list,
+          }
+          return render(request, 'base.html', context)
+
+
+
+class IndexView(View):
+     def get(self, request):
+          return render(request, 'index.html')
